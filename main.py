2fcf5c509626ad1c1dc3182978fa72b7d3c7fd2c
Ruin392 <ruin392@gmail.com>
2022-07-28 21:06:02 +0300

    Home 28.07.22 21:05

--- main.py
+++ main.py
@@ -66,8 +66,8 @@
 # print(process_query('Соня, ты где?'))
 # print(process_query('Коля, что делать?'))
 # print(process_query('Антон, ты где?'))
-import datetime
-import datetime as dt
+# import datetime
+# import datetime as dt
 
 # DATABASE = {
 #     'Серёга': 'Омск',
@@ -213,3 +213,24 @@ import datetime as dt
 #         print(query, '-', process_query(query))
 #
 # runner()
+
+
+# user_query = 'как стать бэкенд-разработчиком'.strip()
+#
+# url = f'https://yandex.ru/search/?text={user_query}'.split()  # ваш код здесь
+# a = '%20'.join(url)
+# print(a)
+
+# import urllib.parse
+# url = 'https://yandex.ru/search/?text=%D0%BA%D0%B0%D0%BA%20%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE%20%D0%B5%D0%B7%D0%B4%D0%B8%D1%82%D1%8C%20%D0%BD%D0%B0%20%D1%82%D0%B0%D0%BA%D1%81%D0' \
+#       '%B8'
+#
+#
+#
+# # чтобы вычленить текст вопроса
+# # разбейте строку по знаку = и возьмите
+# # второй элемент получившегося списка
+# question =  url.split('=')[1]
+#
+# # напечатайте на экран запрос в расшифрованном виде
+# print(urllib.parse.unquote(question))
