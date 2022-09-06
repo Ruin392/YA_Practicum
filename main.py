7ba344170036e83ac4de85e3fc3b0f7e51c0407a
Ruin392 <Ruin392@gmail.com>
2022-09-06 11:02:50 +0300

    МНого изменений но основные это в YA_PRACTICUM

--- main.py
+++ main.py
@@ -536,31 +536,38 @@
 # print(message)
 # calc(25.5)
 
-class Contact:
-    def __init__(self,
-                 name,
-                 year_birth,
-                 is_programmer):
-        self.name = name
-        self.year_birth = year_birth
-        self.is_programmer = is_programmer
-
-    def age_define(self):
-        if 1946 < self.year_birth < 1980:
-            return 'Олдскул'
-        if self.year_birth >= 1980:
-            return 'Молодой'
-        return 'Старейшина'
-
-    def programmer_define(self):
-        if self.is_programmer:
-            return 'Программист'
-        return 'Нормальный'
-
-    def show_contact(self):
-        return(f'{self.name}, '
-               f'категория: {self.age_define()}, '
-               f'статус: {self.programmer_define()}')
-
-    def print_contact(self):
-        print(self.show_contact())
+# class Contact:
+#     Contact: str
+#     name: str
+#     year_birth: int
+#     is_programmer: bool
+#
+#     def __init__(self,
+#                  name: str,
+#                  year_birth: int,
+#                  is_programmer: bool) -> None:
+#         self.name = name
+#         self.year_birth = year_birth
+#         self.is_programmer = is_programmer
+#
+#     def age_define(self) -> str:
+#         if 1946 < self.year_birth < 1980:
+#             return 'Олдскул'
+#         if self.year_birth >= 1980:
+#             return 'Молодой'
+#         return 'Старейшина'
+#
+#     def programmer_define(self) -> str:
+#         if self.is_programmer:
+#             return 'Программист'
+#         return 'Нормальный'
+#
+#     def show_contact(self) -> str:
+#         return (f'{self.name}, '
+#                 f'категория: {self.age_define()}, '
+#                 f'статус: {self.programmer_define()}')
+#
+#     def print_contact(self) -> None:
+#         print(self.show_contact())
+# a = Contact('Мария', 1999, 777)
+# print(a.show_contact())
